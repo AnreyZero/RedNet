3893c3d807286219c809cf4e76ab5012a04addfd
AnreyZero <43420095+AnreyZero@users.noreply.github.com>
2018-09-21 22:36:25 +0400

    Add files via upload

--- Project/jsAuto.js
+++ Project/jsAuto.js
@@ -2,7 +2,15 @@
 // var Familu = document.Familu;
 // var Otchestvo = document.Otchestvo;
 // var Gorod =document.Gorod;
-// document(Name+" "+Familu+" "+Otchestvo+" "+Gorod);
+// document(Name+" "+Familu+" "+Otchestvo+" "+Gorod);// var provName = new Array(5);
+// for(var i=0;i<provName.length;i++){
+// 	provName[i] = Name.substring(i,i+1);
+// 	 if(provName[i]=="#")
+// 	 {
+// 	 	alert(Веден не коректный символ в имени);
+// 	 }
+// }
+// alert(provName[]);
 function autoForm(){
 var Name = document.getElementById("names").value;
 var Familu = document.getElementById("familu").value;
@@ -16,10 +24,9 @@ var Familu = document.getElementById("familu").value;
 	else{
 		alert("А ты собственно кто?");
 	}	
-} 
+}  
 function regForm(){
 	 var rName = document.getElementById("rname").value;
-	 var rFamilu = document.getElemetById("rfamilu").value;
-	 var rOtchest = document.getElemetById("rothestv").value;
-	 var rGorod = document.getElemetById("rgorod").value;
+	 var rPass = document.getElemetById("rpass").value;
+	 var rMail = document.getElemetById("rmail").value;
 }
